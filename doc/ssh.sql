49f5c0d39da9c714801d86fbd55913d89c007ccc
melodyfff <307208327@qq.com>
2017-10-07 02:10:45 +0800

    [ssh-0.0.1-SNAPSHOT] 更新 sql 文件

---
+++ doc/ssh.sql
@@ -0,0 +1,110 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : ssh
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2017-10-07 02:09:06
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for authority
+-- ----------------------------
+DROP TABLE IF EXISTS `authority`;
+CREATE TABLE `authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authority_name` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of authority
+-- ----------------------------
+INSERT INTO `authority` VALUES ('1', '管理员尊贵权限', '/admin');
+INSERT INTO `authority` VALUES ('2', '欢迎页权限', '/welcome');
+INSERT INTO `authority` VALUES ('3', 'test', '/test');
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', 'admin');
+INSERT INTO `role` VALUES ('2', 'user');
+INSERT INTO `role` VALUES ('3', '测试员');
+
+-- ----------------------------
+-- Table structure for role_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `role_authority`;
+CREATE TABLE `role_authority` (
+  `authority_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  KEY `FK_pd9dxrh00ojci21l1h9pj772` (`role_id`),
+  KEY `FK_17v3u9qbwajrm5jsaj4uxujx8` (`authority_id`),
+  CONSTRAINT `FK_17v3u9qbwajrm5jsaj4uxujx8` FOREIGN KEY (`authority_id`) REFERENCES `authority` (`id`),
+  CONSTRAINT `FK_pd9dxrh00ojci21l1h9pj772` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_authority
+-- ----------------------------
+INSERT INTO `role_authority` VALUES ('1', '1');
+INSERT INTO `role_authority` VALUES ('2', '1');
+INSERT INTO `role_authority` VALUES ('2', '2');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `register_time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('25', 'admin', 'admin', '2017-10-03');
+INSERT INTO `user` VALUES ('26', 'xinchen', '123', '2017-10-05');
+INSERT INTO `user` VALUES ('27', 'test', 'test', '2017-10-05');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  KEY `FK_it77eq964jhfqtu54081ebtio` (`role_id`),
+  KEY `FK_apcc8lxk2xnug8377fatvbn04` (`user_id`),
+  CONSTRAINT `FK_apcc8lxk2xnug8377fatvbn04` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_it77eq964jhfqtu54081ebtio` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES ('25', '1');
+INSERT INTO `user_role` VALUES ('25', '2');
+INSERT INTO `user_role` VALUES ('26', '2');
